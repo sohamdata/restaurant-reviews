3a918e9ed855d55ef4c367b1e444a147e5b1d151
Soham Datta <sohamdatta34@gmail.com>
2022-10-17 19:14:12 +0530

    added restaurant cards and reviews

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:3001/api/v1/restaurants",
+    baseURL: "http://localhost:5000/api/v1/restaurants", // check the port number in env file
     headers: {
         "Content-type": "application/json"
     }
